9fe9ea89ad2eccaefb0e195216aaca2060aa79cd
Milan Lenco <milan.lenco@pantheon.tech>
2018-05-15 10:22:45 +0200

    Pull nat-test vswitch.

--- k8s/pull-images.sh
+++ k8s/pull-images.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker pull contivvpp/vswitch:latest
+docker pull contivvpp/vswitch:nat-test
 #docker pull contivvpp/cri:latest
 docker pull contivvpp/ksr:latest
 docker pull contivvpp/cni:latest
